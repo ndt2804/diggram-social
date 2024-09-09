37678b4df32a61e314fcc025cdab588c85bacc5d
ndt2804 <nduytan.uit.a3@gmail.com>
2024-09-10 02:58:08 +0700

    feat: add cors

--- social-backend/src/server.js
+++ social-backend/src/server.js
@@ -2,9 +2,16 @@ import route from "../api/route.js";
 import bodyParser from "body-parser";
 
 import express from "express";
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
 app.use(express({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 import "dotenv/config";
